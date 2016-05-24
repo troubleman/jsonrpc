457d363a9ef50ed02d6259bb287cabb45420c535
Newiep <674898224@qq.com>
2016-05-24 18:42:26 +0800

    some code

--- src/ProcedureHandler.php
+++ src/ProcedureHandler.php
@@ -6,5 +6,56 @@ namespace JsonRPC;
 
 class ProcedureHandler
 {
+    /**
+     * List of procedures
+     *
+     * @var array
+     */
+    private $callbacks = array();
 
+    /**
+     * List of classes
+     *
+     * @var array
+     */
+    private $classes = array();
+
+    /**
+     * List of instances
+     *
+     * @var array
+     */
+    private $instances = array();
+
+    /**
+     * Register a new procedure
+     *
+     * @param $procedure
+     * @param \Closure $callback
+     *
+     * @return $this
+     */
+    public function withCallback($procedure, \Closure $callback)
+    {
+        $this->callbacks[$procedure] = $callback;
+        return $this;
+    }
+
+    /**
+     * Bind a class instance
+     * 
+     * @param $instance
+     *
+     * @return $this
+     */
+    public function withInstance($instance)
+    {
+        $this->instances[] = $instance;
+        return $this;
+    }
+
+    public function executeProcedure($procedure, array $params = array())
+    {
+
+    }
 }
--- src/Server.php
+++ src/Server.php
@@ -3,8 +3,51 @@
 
 namespace JsonRPC;
 
+use JsonRPC\ProcedureHandler;
 
 class Server
 {
+    protected $payload;
+
+    protected $procedureHandler;
+
+
+    public function __construct($request = '')
+    {
+        if ($request !== '') {
+            $this->payload = json_decode($request, true);
+        } else {
+            $this->payload = json_decode(file_get_contents('php://input'), true);
+        }
+
+        $this->procedureHandler = new ProcedureHandler();
+    }
+
+    /**
+     * Register a new procedure
+     *
+     * @param $procedure
+     * @param \Closure $callback
+     *
+     * @return $this
+     */
+    public function register($procedure, \Closure $callback)
+    {
+        $this->procedureHandler->withCallback($procedure, $callback);
+        return $this;
+    }
+
+    /**
+     * Bind a class instance
+     *
+     * @param $instance  class instance or name
+     *
+     * @return $this
+     */
+    public function attach($instance)
+    {
+        $this->procedureHandler->withInstance($instance);
+    }
+
 
 }
